bcbdfcbbe45cbdd7eb1129bafbb1606f7c1bb4df
dmmjy9 <dmmjy9@gmail.com>
2020-07-02 23:35:16 +0800

    ch2_container done

---
+++ container/maps.go
@@ -0,0 +1,34 @@
+package container
+
+import "fmt"
+
+func main() {
+	m := map[string]string {
+		"name": "ccmouse",
+		"course": "golang",
+		"site": "imooc",
+		"quality": "notbad",
+	}
+	m2 := make(map[string]int)
+	var m3 map[string]int
+	fmt.Println(m, m2, m3)
+
+	fmt.Println("Traversing map")
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	fmt.Println("Getting values")
+	if courseName, ok := m["course"]; ok {
+		fmt.Println(courseName)
+	} else {
+		fmt.Println("key does not exist")
+	}
+
+	fmt.Println("Deleting values")
+	name, ok := m["name"]
+	fmt.Println(name, ok)
+	delete(m, "name")
+	name, ok = m["name"]
+	fmt.Println(name, ok)
+}
